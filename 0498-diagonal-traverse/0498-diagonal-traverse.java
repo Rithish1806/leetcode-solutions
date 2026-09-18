cd1fe9cbc4e7de766d8ed06d9ba9e1e301346a9a
RITHISH M <rithishm2006@gmail.com>
2026-09-18 15:54:31 +0530

    Time: 2 ms (98.78%), Space: 48.3 MB (30.82%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        if(mat.length == 0){
-            return new int[0];
-        }
-        int r =mat.length;
-        int c =mat[0].length;
-        int[] result = new int[r*c];
+        if(mat.length == 0)return new int[0];
+        int r = mat.length;
+        int c = mat[0].length;
         int x=0,y=0;
+        int[] result = new int[r*c];
         for(int i=0;i<result.length;i++){
             result[i] = mat[x][y];
             if((x+y) % 2 == 0){
-                if(y == c-1){
+                if(y == c-1) {
                     x++;
                 }
-                else if(x == 0){
+                else if(x==0){
                     y++;
                 }
                 else{
@@ -25,13 +23,13 @@ class Solution {
                 if(x == r-1){
                     y++;
                 }
-                else if(y == 0){
+                else if(y==0){
                     x++;
                 }
                 else{
                     x++;
-                    y--;    
-                }   
+                    y--;
+                }
             }
         }
         return result;
