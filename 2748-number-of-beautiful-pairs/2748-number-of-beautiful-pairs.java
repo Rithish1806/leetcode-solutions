8c4780cd7f9538bc1ec241d59b00f68a07fdd05a
RITHISH M <rithishm2006@gmail.com>
2026-09-20 23:15:02 +0530

    Time: 63 ms (6.74%), Space: 46.7 MB (14.45%) - LeetHub

---
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countBeautifulPairs(int[] nums) {
+        int count = 0;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j = i+1;j<nums.length;j++){
+                String s1 = String.valueOf(nums[i]);
+                String s2 = String.valueOf(nums[j]);
+                int a = s1.charAt(0)-'0';
+                int b = s2.charAt(s2.length()-1) -'0';
+                while(b!=0){
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
+                }
+                if(a==1){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
